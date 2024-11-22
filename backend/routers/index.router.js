3bd7cf126b0d7c54a105703e031fbc9348a0b45c
nguyennt <nguyentrungnguyen12a3@gmail.com>
2024-11-22 20:08:42 +0700

    feat(app): add notification CRUD, profile api

--- backend/routers/index.router.js
+++ backend/routers/index.router.js
@@ -1,9 +1,11 @@
-import express from 'express'
-import authRouter from '../routers/auth.router.js'
-import contentRouter from '../routers/content.router.js'
-const router = express.Router()
+import express from "express";
+import authRouter from "../routers/auth.router.js";
+import contentRouter from "../routers/content.router.js";
+import profileRouter from "../routers/profile.router.js";
+const router = express.Router();
 
-router.use('/auth',authRouter)
-router.use('/content', contentRouter)
+router.use("/auth", authRouter);
+router.use("/content", contentRouter);
+router.use("/profile", profileRouter);
 
-export default router
+export default router;
