0de7351e7d32ace9aa7e329c39c81fbf81f86695
Hoanghaaii <haivu04112003@gmail.com>
2024-11-25 14:31:39 +0700

    fix watch history

--- backend/routers/watchHistory.router.js
+++ backend/routers/watchHistory.router.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import { verifyToken } from '../middleware/verifyToken.js'
-import { createWatchHistory, updateProgressWatchHistory } from '../controllers/watchHistory.controller.js'
+import { createWatchHistory, deleteWatchHistory, getWatchHistoryByUserId, updateProgressWatchHistory } from '../controllers/watchHistory.controller.js'
 
 const router = express.Router()
 
@@ -8,6 +8,8 @@ router.post('/create-watch-history', verifyToken, createWatchHistory)
 
 router.post('/update-progress', verifyToken,updateProgressWatchHistory)
 
-router.get('/get-watch-histories')
+router.get('/get-watch-histories', verifyToken, getWatchHistoryByUserId)
+
+router.delete('/delete-watch-history/:watchHistoryId', verifyToken, deleteWatchHistory)
 
 export default router
