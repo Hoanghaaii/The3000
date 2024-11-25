5c591fcd04c7b64b1d08ca5e428f468c5abeda7a
BanKan123123 <bankankis@gmail.com>
2024-11-25 14:36:17 +0700

    Ninhnv update

--- backend/routers/auth.router.js
+++ backend/routers/auth.router.js
@@ -1,12 +1,14 @@
 import express from 'express'
 import { checkver, login, signup, sendVerifyEmail, verifyEmail } from '../controllers/auth.controller.js'
-import {verifyToken} from '../middleware/verifyToken.js'
+import { verifyToken } from '../middleware/verifyToken.js'
 import { refreshAccessToken } from '../utils/generateTokenAndSetCookie.js'
 const router = express.Router()
 
 router.post('/signup', signup)
-router.post('/login',login)
-router.get('/get-profile',verifyToken,checkver)
+router.post('/login', login)
+router.get('/get-profile', verifyToken, checkver)
+// router.get('/get-profile', checkver)
+
 router.get('/refresh-token', refreshAccessToken)
 router.post('/send-verify-email', sendVerifyEmail)
 router.post('/verify-email', verifyEmail)
